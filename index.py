4bcf045c6aab82255e58fc159d4754de222e7d0b
listenfree <chowchi@gmail.com>
2020-08-25 10:48:24 +0800

    Update index.py

--- index.py
+++ index.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-
-@app.route('/1.jpg')
-def getimage():
     image_binary = get("https://loremflickr.com/600/400").content
     response = make_response(image_binary)
     response.headers.set('Content-Type', 'image/jpeg')
     return response
+    #return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
