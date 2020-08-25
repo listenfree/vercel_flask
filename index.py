2fd8165aa0f65b817f644bd9ca7aeeaf0a34ab4b
listenfree <chowchi@gmail.com>
2020-08-25 11:34:38 +0800

    Update index.py

--- index.py
+++ index.py
@@ -5,17 +5,11 @@ import gevent
 app = Flask(__name__)
 sockets = Sockets(app)
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     image_binary = get("https://loremflickr.com/600/400").content
-#     response = make_response(image_binary)
-#     response.headers.set('Content-Type', 'image/jpeg')
-#     return response
-#     #return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-
-@sockets.route('/')
-def eeee_socket(ws):
-    while not ws.closed:
-        message = ws.receive()
-        ws.send(message)
+@app.route('/i', defaults={'path': ''})
+@app.route('/i/<path:path>')
+def catch_all(path):
+    image_binary = get("https://loremflickr.com/600/400").content
+    response = make_response(image_binary)
+    response.headers.set('Content-Type', 'image/jpeg')
+    return response
+    #return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
